fc1f1896c96cde0dbdb383a6ec0538342eadb03a
Tiago Pereira <tiagopereiraduarte8@gmail.com>
2025-02-05 16:46:49 -0300

    inclusao do authorize

--- DevSteamAPI/Controllers/JogosController.cs
+++ DevSteamAPI/Controllers/JogosController.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DevSteamAPI.Data;
 using DevSteamAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DevSteamAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class JogosController : ControllerBase
     {
         private readonly DevSteamAPIContext _context;
